a5210f704a9b8c49e24188babb45f9f50e67823f
smj187 <engulf_clamshell_joining_repulsion@proton.me>
2022-12-09 23:51:56 +0100

    init backend

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ```
 docker-compose up --build
 ```
+
+## backend virtual environment
+
+```
+cd backend
+
+python3 -m venv backend-env
+.\backend-env\Scripts\Activate.ps1
+
+pip3 freeze > requirements.txt
+
+uvicorn main:app --host localhost --port 8000 --reload
+```
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+
 export const App: React.FC = () => {
+  const load = async () => {
+    const r = await fetch("http://localhost:8000")
+    console.log(await r.json())
+  }
+
+  useEffect(() => {
+    load()
+  }, [])
   return <div className="bg-zinc-900 text-white">test</div>
 }
 
