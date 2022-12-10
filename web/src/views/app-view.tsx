8f3fbd4196cec220570dc63e514fa03229d0c71f
smj187 <engulf_clamshell_joining_repulsion@proton.me>
2022-12-10 13:36:22 +0100

    update header

--- web/src/views/app-view.tsx
+++ web/src/views/app-view.tsx
@@ -1,3 +1,40 @@
+import { useState } from "react"
+
 export const AppView: React.FC = () => {
-  return <div>app</div>
+  const [selected, setSelected] = useState("Learning")
+  return (
+    <div className="font-inter">
+      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+        <ul className="flex flex-wrap -mb-px">
+          <li className="mr-2">
+            <a
+              onClick={() => setSelected("Learning")}
+              href="#"
+              className={
+                selected === "Learning"
+                  ? "inline-block p-4 text-rose-600 rounded-t-lg border-b-2 border-rose-600 active"
+                  : "inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300"
+              }
+            >
+              Learning
+            </a>
+          </li>
+          <li className="mr-2">
+            <a
+              onClick={() => setSelected("My Files")}
+              href="#"
+              className={
+                selected === "My Files"
+                  ? "inline-block p-4 text-rose-600 rounded-t-lg border-b-2 border-rose-600 active"
+                  : "inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300"
+              }
+              aria-current="page"
+            >
+              My Files
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./src/**/*.{vue,ts,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"]
+      },
+      maxWidth: {
+        "8xl": "1440px"
+      }
+    }
   },
   plugins: []
 }
+
